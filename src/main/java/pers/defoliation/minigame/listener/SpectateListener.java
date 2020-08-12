4788dc28b17596962a94fc2e937d5cf718b6d84c
defoliation <defoli_ation@outlook.com>
2020-08-12 15:28:12 +0800

    SpectateListener update

--- src/main/java/pers/defoliation/minigame/listener/SpectateListener.java
+++ src/main/java/pers/defoliation/minigame/listener/SpectateListener.java
@@ -1,142 +1,101 @@
 package pers.defoliation.minigame.listener;
 
-import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftInventoryPlayer;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
+import org.bukkit.event.*;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerPickupItemEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.plugin.IllegalPluginAccessException;
+import org.bukkit.plugin.RegisteredListener;
+import org.bukkit.plugin.java.JavaPlugin;
 import pers.defoliation.minigame.player.GamePlayer;
 
-import static org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * 如果需要使用，则自己注册
  */
 public class SpectateListener implements Listener {
 
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerTeleport(PlayerTeleportEvent e) {
-        Player player = e.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (gamePlayer.isSpectator() && e.getCause() != PLUGIN)
-            e.setCancelled(true);
+    private HashMap<Class, Function<Event, Player>> event2PlayerMap = new HashMap<>();
+
+    public SpectateListener() {
+        registerEvent2Player(PlayerEvent.class, PlayerEvent::getPlayer);
+        registerEvent2Player(BlockPlaceEvent.class, BlockPlaceEvent::getPlayer);
+        registerEvent2Player(BlockBreakEvent.class, BlockBreakEvent::getPlayer);
+        registerEvent2Player(InventoryOpenEvent.class, event -> (Player) event.getPlayer());
+        registerEvent2Player(InventoryClickEvent.class, event -> (Player) event.getWhoClicked());
     }
 
-    @EventHandler
-    public void interact(PlayerInteractEvent interactEvent) {
-        Player player = (Player) interactEvent.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        int itemInHandIndex = ((CraftInventoryPlayer) player.getInventory()).getInventory().itemInHandIndex;
-        interactEvent.setCancelled(true);
-        if ((interactEvent.getAction() == Action.RIGHT_CLICK_AIR || interactEvent.getAction() == Action.RIGHT_CLICK_BLOCK)
-                && interactEvent.getItem() != null) {
-            slotCommand(player, itemInHandIndex);
+
+    public <T extends Event> void registerEvent2Player(Class<T> tClass, Function<T, Player> playerFunction) {
+        event2PlayerMap.put(tClass, (Function<Event, Player>) playerFunction);
+    }
+
+    public <T extends Event> SpectateListener addHandle(JavaPlugin plugin, Class<T> event, Consumer<T> consumer) {
+        return addHandle(plugin, event, consumer, false);
+    }
+
+    public <T extends Event> SpectateListener addHandle(JavaPlugin plugin, Class<T> event, Consumer<T> consumer, boolean ignoreCancel) {
+        getEventListeners(event).register(new RegisteredListener(new Listener() {
+        }, (listener, event1) -> {
+            if (!(event1 instanceof Cancellable) || !((Cancellable) event1).isCancelled() || !ignoreCancel) {
+                Function<Event, Player> function = getFunction(event1.getClass());
+                if (function == null)
+                    return;
+                Player apply = function.apply(event1);
+                if (apply != null && apply.isOnline()) {
+                    GamePlayer gamePlayer = GamePlayer.getGamePlayer(apply);
+                    if (gamePlayer.isSpectator()) {
+                        consumer.accept((T) event1);
+                    }
+                }
+            }
+        }, EventPriority.NORMAL, plugin, ignoreCancel));
+        return this;
+    }
+
+    private Function<Event, Player> getFunction(Class clazz) {
+        if (clazz == null || clazz.equals(Object.class))
+            return null;
+        if (event2PlayerMap.containsKey(clazz))
+            return event2PlayerMap.get(clazz);
+        return getFunction(clazz.getSuperclass());
+    }
+
+    public static <T extends Event> Consumer<T> cancel() {
+        return event -> {
+            if (event instanceof Cancellable)
+                ((Cancellable) event).setCancelled(true);
+        };
+    }
+
+    private HandlerList getEventListeners(Class<? extends Event> type) {
+        try {
+            Method method = this.getRegistrationClass(type).getDeclaredMethod("getHandlerList");
+            method.setAccessible(true);
+            return (HandlerList) method.invoke((Object) null);
+        } catch (Exception var3) {
+            throw new IllegalPluginAccessException(var3.toString());
         }
     }
 
-    @EventHandler
-    public void placeBlock(BlockPlaceEvent placeEvent) {
-        Player player = (Player) placeEvent.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
+    private Class<? extends Event> getRegistrationClass(Class<? extends Event> clazz) {
+        try {
+            clazz.getDeclaredMethod("getHandlerList");
+            return clazz;
+        } catch (NoSuchMethodException var2) {
+            if (clazz.getSuperclass() != null && !clazz.getSuperclass().equals(Event.class) && Event.class.isAssignableFrom(clazz.getSuperclass())) {
+                return this.getRegistrationClass(clazz.getSuperclass().asSubclass(Event.class));
+            } else {
+                throw new IllegalPluginAccessException("Unable to find handler list for event " + clazz.getName() + ". Static getHandlerList method required!");
+            }
         }
-        placeEvent.setCancelled(true);
-    }
-
-    @EventHandler
-    public void breakBlock(BlockBreakEvent breakEvent) {
-        Player player = (Player) breakEvent.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        breakEvent.setCancelled(true);
-    }
-/*
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent e) {
-        Player player = e.getPlayer();
-        GameMap gameMap = MatchManager.get().getSpectatorMap(player);
-        if (gameMap == null) {
-            return;
-        }
-        gameMap.getSpectators().remove(player.getUniqueId());
-        gameMap.getAlivePlayers().remove(player);
-        gameMap.getAllPlayers().remove(player);
-        MatchManager.get().removeSpectator(player);
-    }*/
-
-    @EventHandler
-    public void openInventory(InventoryOpenEvent openEvent) {
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(openEvent.getPlayer().getName());
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        openEvent.setCancelled(true);
-    }
-
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void onInventoryClick(InventoryClickEvent e) {
-        Player player = (Player) e.getWhoClicked();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        e.setCancelled(true);
-    }
-
-    private void slotCommand(Player player, int slot) {
-    /*    List<String> strings = MatchManager.get().spectateCommands.get(slot);
-        if (strings != null) {
-            strings.stream().map(s -> s.replace("%player%", player.getName()))
-                    .forEach(command -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command));
-        }*/
-    }
-
-/*    @EventHandler(priority = EventPriority.NORMAL)
-    public void onPlayerMove(PlayerMoveEvent e) {
-        Player player = (Player) e.getPlayer();
-        GameMap gameMap = MatchManager.get().getSpectatorMap(player);
-        if (gameMap == null) {
-            return;
-        }
-        if (player.getLocation().getY() < 0) {
-            CoordLoc spectateSpawn = gameMap.getSpectateSpawn();
-            player.teleport(new Location(gameMap.getCurrentWorld(), spectateSpawn.getX(), spectateSpawn.getY(), spectateSpawn.getZ()), END_PORTAL);
-        }
-    }*/
-
-    @EventHandler
-    public void onDrop(PlayerDropItemEvent event) {
-        Player player = (Player) event.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        event.setCancelled(true);
-    }
-
-    @EventHandler
-    public void onPickup(PlayerPickupItemEvent event) {
-        Player player = (Player) event.getPlayer();
-        GamePlayer gamePlayer = GamePlayer.getGamePlayer(player);
-        if (!gamePlayer.isSpectator()) {
-            return;
-        }
-        event.setCancelled(true);
     }
 
 }
