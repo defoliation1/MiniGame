81204a5e895247fad30b8f561df62559bdd73ac6
defoliation <defoli_ation@outlook.com>
2020-08-14 19:08:18 +0800

    update Game.java

--- src/main/java/pers/defoliation/minigame/game/Game.java
+++ src/main/java/pers/defoliation/minigame/game/Game.java
@@ -1,19 +1,60 @@
 package pers.defoliation.minigame.game;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import pers.defoliation.minigame.group.GamePlayerGroup;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public interface Game {
+public abstract class Game {
 
-    GameState preparing(AtomicInteger time);
+    private String gameName;
 
-    GameState waiting(AtomicInteger time);
+    public Game(String gameName) {
+        this.gameName = gameName;
+    }
 
-    GameState running(AtomicInteger time);
+    public String getGameName() {
+        return gameName;
+    }
 
-    GameState ended(AtomicInteger time);
+    public YamlConfiguration getGameConfig() {
+        YamlConfiguration yamlConfiguration = new YamlConfiguration();
+        File gameConfigFile = getGameConfigFile();
+        if (!gameConfigFile.exists()) {
+            gameConfigFile.getParentFile().mkdirs();
+            try {
+                gameConfigFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            yamlConfiguration.load(gameConfigFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        return yamlConfiguration;
+    }
 
-    GamePlayerGroup getGroup();
+    private File getGameConfigFile() {
+        return new File(getDataFolder(), "game" + File.pathSeparator + gameName + ".yml");
+    }
+
+    public abstract File getDataFolder();
+
+    public abstract GameState preparing(AtomicInteger time);
+
+    public abstract GameState waiting(AtomicInteger time);
+
+    public abstract GameState running(AtomicInteger time);
+
+    public abstract GameState ended(AtomicInteger time);
+
+    public abstract GamePlayerGroup getGroup();
 
 }
