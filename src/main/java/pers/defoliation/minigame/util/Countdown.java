0b5e11b1ed6fb810c56ef3863fc6402e88eb72ca
defoliation <defoli_ation@outlook.com>
2020-08-21 17:06:51 +0800

    update

--- src/main/java/pers/defoliation/minigame/util/Countdown.java
+++ src/main/java/pers/defoliation/minigame/util/Countdown.java
@@ -1,5 +1,8 @@
 package pers.defoliation.minigame.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -18,32 +21,37 @@ public class Countdown<T> {
 
     private HashMap<Integer, BiConsumer<T, AtomicInteger>> secondTask = new HashMap<>();
 
-    private int tick;
-
-    private Countdown() {
-    }
-
-    public void tick() {
-        tick++;
-        if (tick % 20 == 0) {
-            second();
-        }
+    private Countdown(JavaPlugin plugin) {
+        Bukkit.getScheduler().runTaskTimer(plugin, this::second, 20, 20);
     }
 
     public void second() {
+        List<CountdownData> removeList = new ArrayList<>();
         for (CountdownData countdownData : countdownDataList) {
-            int i = countdownData.atomicInteger.incrementAndGet();
-            if (i <= 0) {
+            if (countdownData.atomicInteger.incrementAndGet() <= 0) {
                 whenEnd.accept(countdownData.t);
             } else {
                 perSecondTask.forEach(consumer -> consumer.accept(countdownData.t, countdownData.atomicInteger));
             }
+            if (countdownData.atomicInteger.get() <= 0) {
+                removeList.add(countdownData);
+            }
         }
+        countdownDataList.remove(removeList);
     }
 
     public void start(int countdownTime, T... t) {
         for (T t1 : t) {
             countdownDataList.add(new CountdownData(countdownTime, t1));
+        }
+    }
+
+    public void cancel(T t) {
+        for (CountdownData countdownData : countdownDataList) {
+            if(countdownData.t.equals(t)){
+                countdownDataList.remove(countdownData);
+                return;
+            }
         }
     }
 
@@ -63,13 +71,17 @@ public class Countdown<T> {
         }
     }
 
-    public static <T> CountdownBuilder<T> getBuilder() {
-        return new CountdownBuilder<T>();
+    public static <T> CountdownBuilder<T> getBuilder(JavaPlugin plugin) {
+        return new CountdownBuilder<T>(plugin);
     }
 
     public static class CountdownBuilder<T> {
 
         private Countdown<T> countdown;
+
+        public CountdownBuilder(JavaPlugin plugin) {
+            this.countdown = new Countdown<>(plugin);
+        }
 
         public CountdownBuilder<T> whenEnd(Consumer<T> consumer) {
             countdown.whenEnd = consumer;
