7e7f5715ef833edfb74f1ae591c0ea4c96abb967
defoliation <defoli_ation@outlook.com>
2020-08-21 15:11:10 +0800

    update

--- src/main/java/pers/defoliation/minigame/util/Countdown.java
+++ src/main/java/pers/defoliation/minigame/util/Countdown.java
@@ -1,35 +1,23 @@
 package pers.defoliation.minigame.util;
 
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import org.bukkit.entity.Player;
-import pers.defoliation.minigame.group.GamePlayerGroup;
-import pers.defoliation.minigame.group.Team;
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.IntStream;
 
-public class Countdown {
+public class Countdown<T> {
 
-    private int countdown = Integer.MAX_VALUE;
-    private Supplier<Integer> countdownTime;
-    private Supplier<Boolean> canCountdown;
-    private List<Consumer<Integer>> perSecondTask = new ArrayList<>();
-    private Multimap<Integer, Runnable> secondTask = HashMultimap.create();
+    private List<CountdownData> countdownDataList = new ArrayList<>();
+
+    private Consumer<T> whenEnd;
+
+    private List<BiConsumer<T, AtomicInteger>> perSecondTask = new ArrayList<>();
+
     private int tick;
 
-    public Countdown(Supplier<Integer> countdownTime, Supplier<Boolean> canCountdown) {
-        this.countdownTime = countdownTime;
-        this.canCountdown = canCountdown;
-    }
-
-    public void resetCountdown() {
-        countdown = this.countdownTime.get();
+    private Countdown() {
     }
 
     public void tick() {
@@ -39,66 +27,62 @@ public class Countdown {
         }
     }
 
-    public Countdown addPerSecondTask(Consumer<Integer> secondConsumer) {
-        perSecondTask.add(secondConsumer);
-        return this;
-    }
-
-    public Countdown setSecondTask(int second, Runnable task) {
-        secondTask.put(second, task);
-        return this;
-    }
-
-    public Countdown setSecondTask(Consumer<Integer> consumer, int... seconds) {
-        if (seconds == null || seconds.length == 0)
-            return this;
-        Arrays.sort(seconds);
-        addPerSecondTask(integer -> {
-            if (Arrays.binarySearch(seconds, integer) > 0) {
-                consumer.accept(integer);
-            }
-        });
-        return this;
-    }
-
     public void second() {
-        int countdownTime = this.countdownTime.get();
-        if (canCountdown.get()) {
-            countdown--;
-            if (countdown > countdownTime)
-                countdown = countdownTime;
-        } else {
-            countdown = countdownTime;
-        }
-        perSecondTask.forEach(consumer -> consumer.accept(countdownTime));
-        secondTask.get(countdownTime).forEach(Runnable::run);
-    }
-
-    /**
-     * 策略1
-     *
-     * @param countdownTime           当玩家数量达到最低要求时的倒计时时间
-     * @param fullCountdownTime       当玩家数量满时的倒计时时间
-     * @param group                   用于判断的玩家群体
-     * @param startCountdownPlayerNum 玩家数量的最低要求
-     * @return
-     */
-    public static Countdown speedUpWhenFull(int countdownTime, int fullCountdownTime, GamePlayerGroup<? extends Team> group, int startCountdownPlayerNum) {
-        return new Countdown(() -> {
-            if (group.playerNum() == group.getTeams().stream().flatMapToInt(team -> IntStream.of(team.getMaxPlayer())).sum()) {
-                return fullCountdownTime;
+        for (CountdownData countdownData : countdownDataList) {
+            int i = countdownData.atomicInteger.incrementAndGet();
+            if (i <= 0) {
+                whenEnd.accept(countdownData.t);
             } else {
-                return countdownTime;
+                perSecondTask.forEach(consumer -> consumer.accept(countdownData.t, countdownData.atomicInteger));
             }
-        }, () -> group.playerNum() >= startCountdownPlayerNum);
+        }
     }
 
-    public static Consumer<Integer> setLevel(Supplier<List<Player>> players) {
-        return integer -> players.get().forEach(player -> player.setLevel(integer));
+    public void start(int countdownTime, T... t) {
+        for (T t1 : t) {
+            countdownDataList.add(new CountdownData(countdownTime, t1));
+        }
     }
 
-    public static Consumer<Integer> sendTitle(Supplier<List<Player>> players, Function<Integer, Title> function) {
-        return integer -> function.apply(integer).send(players.get());
+    public void start(int countdownTime, Collection<T> collection) {
+        for (T t : collection) {
+            countdownDataList.add(new CountdownData(countdownTime, t));
+        }
     }
 
+    private class CountdownData {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        T t;
+
+        public CountdownData(int time, T t) {
+            atomicInteger.set(time);
+            this.t = t;
+        }
+    }
+
+    public static <T> CountdownBuilder<T> getBuilder() {
+        return new CountdownBuilder<T>();
+    }
+
+    public static class CountdownBuilder<T> {
+
+        private Countdown<T> countdown;
+
+        public CountdownBuilder<T> whenEnd(Consumer<T> consumer) {
+            countdown.whenEnd = consumer;
+            return this;
+        }
+
+        public CountdownBuilder<T> addPerSecondTask(BiConsumer<T, AtomicInteger>... consumers) {
+            for (BiConsumer<T, AtomicInteger> consumer : consumers) {
+                countdown.perSecondTask.add(consumer);
+            }
+            return this;
+        }
+
+        public Countdown<T> build() {
+            return countdown;
+        }
+
+    }
 }
