a624fb9febabeab1f6b6840a69a4be4d28822052
defoliation <defoli_ation@outlook.com>
2020-08-22 19:57:24 +0800

    update region

---
+++ src/main/java/pers/defoliation/minigame/map/Region.java
@@ -0,0 +1,13 @@
+package pers.defoliation.minigame.map;
+
+import org.bukkit.Location;
+
+public interface Region {
+
+    void reset();
+
+    Location getLocationA();
+
+    Location getLocationB();
+
+}
---
+++ src/main/java/pers/defoliation/minigame/map/RegionFactory.java
@@ -0,0 +1,19 @@
+package pers.defoliation.minigame.map;
+
+import org.bukkit.Location;
+
+public final class RegionFactory {
+
+    private RegionFactory() {
+    }
+
+    public static Region getWEResetRegion(Location locationA,Location locationB){
+        return null;
+    }
+
+    public static Region getUnsaveRegion(Location locationA,Location locationB){
+        return null;
+    }
+
+
+}
